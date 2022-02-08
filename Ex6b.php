8851c1d977e86df455d4cbc656ceebd44faf6b2f
Vlad <32599806+Vlad-34@users.noreply.github.com>
2022-02-08 12:09:52 +0200

    Update Ex6b.php

--- Ex6b.php
+++ Ex6b.php
@@ -1,68 +1,63 @@
 <html>
-<style>
-	body
-	{
-		background-image:url("wallpaper.jpg");
-		background-repeat:no-repeat;
-		background-attachment:fixed;
-		background-size:cover;
-	}
+	<style>
+		body
+		{
+			background-image:url("wallpaper.jpg");
+			background-repeat:no-repeat;
+			background-attachment:fixed;
+			background-size:cover;
+		}
+		
+		h1
+		{
+			color:#FF5722;
+			text-align:center;
+			font-size:30px;
+			font-family:Verdana;
+		}
+		
+		th
+		{
+			color: #FF5722;
+		}
+		
+		table, td
+		{
+			border: 2px solid black;
+		}
+	</style>
 	
-	h1
-	{
-		color:#FF5722;
-		text-align:center;
-		font-size:30px;
-		font-family:Verdana;
-	}
-	
-	th
-	{
-		color: #FF5722;
-	}
-	
-	table, td
-	{
-		border: 2px solid black;
-	}
-</style>
-	
-    <head>
-        <title>Exercitiu 6.b)</title>
-    </head>
-    <body>
-        <h1>6.b) Sa se gasească numarul de pagini minim, mediu și maxim pentru fiecare gen.</h1>
-        <?php
-            // se precizează că se foloseşte PEAR DB
-            require_once('PEAR.php');
+	<head>
+		<title>Exercitiu 6.b)</title>
+	</head>
+	<body>
+		<h1>6.b) Sa se gaseasca numarul de pagini minim, mediu și maxim pentru fiecare gen.</h1>
+		<?php
+			// se precizeaza ca se foloseste PEAR DB
+			require_once('PEAR.php');
 			$host = 'localhost';
-            $user = 'UrsacheVlad';
+			$user = 'UrsacheVlad';
 			$pass = file_get_contents("C:/xampp/htdocs/pass.txt");
-            $db_name = 'final';
-            // se stabileşte şirul pentru conexiune universală sau DSN
+			$db_name = 'final';
+			// se stabileste sirul pentru conexiune universala sau DSN
 			$dsn= new mysqli($host, $user, $pass, $db_name);
-            // conectare la BD
-            if ($dsn->connect_error)
-			{
+			if ($dsn->connect_error)
 				die('Eroare la conectare:'. $dsn->connect_error);
-			}
-            // se emite interogarea
-            $query = 'CALL Ex6b()';
-            $result = mysqli_query($dsn, $query);
-            // verifică dacă rezultatul este în regulă
-            if (!$result)
-            {
+			// se emite interogarea
+			$query = 'CALL Ex6b()';
+			$result = mysqli_query($dsn, $query);
+			// verifica daca rezultatul este in regula
+			if (!$result)
 				die('Interogare gresita :'.mysqli_error($dsn));
-            }
-            // se obţine numărul tuplelor returnate
-            $num_results = mysqli_num_rows($result);
-            // se afişează fiecare tuplă returnată
+			// se obtine numarul tuplelor returnate
+			$num_results = mysqli_num_rows($result);
+			// se afiseaza fiecare tupla returnata
 			echo '<table style = "width:100%; background-color: #F8F8FF;">
 			<tr>
-			 <th>MIN(NR_PAGINI)</th>
-			 <th>AVG(NR_PAGINI)</th>
-			 <th>MAX(NR_PAGINI)</th>
-			 <th>GEN</th>
+				<th>MIN(NR_PAGINI)</th>
+				<th>AVG(NR_PAGINI)</th>
+				<th>MAX(NR_PAGINI)</th>
+				<th>GEN</th>
 			</tr>';
 			for ($i=0; $i <$num_results; $i++)
 			{
@@ -73,10 +68,10 @@
 				echo '<td>'.htmlspecialchars(stripslashes($row['gen'])).'</td>';
 			}
 			echo '</table>';
-            // deconectarea de la BD
-            mysqli_close($dsn);
-            ?>
+			// deconectarea de la BD
+			mysqli_close($dsn);
+		?>
 		<a href="index.html" style="cursor:default;">
 		<img src="back.png" style="width:75px; height:75px; margin-top:0.5%;"></a>
-    </body>
+	</body>
 </html>
